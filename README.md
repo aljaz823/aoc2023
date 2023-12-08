f6f5c19e16523f9e1a83e4f16c298a52e2888bf1
aljaz <aljaz.pungercar@agilcon.com>
2023-12-08 09:44:59 +0100

    day 8

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
-![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
@@ -111,16 +111,16 @@ Both parts: 12.145ms
 
 ```
 Day 07
-Time part 1: 6.274ms
-Time part 2: 22.432ms
-Both parts: 28.706ms
+Time part 1: 6.38ms
+Time part 2: 22.475ms
+Both parts: 28.855ms
 ```
 
 ```
 Day 08
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 2.198ms
+Time part 2: 6.736ms
+Both parts: 8.934ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 14/50
-Total time: 759.182ms
+Total stars: 16/50
+Total time: 768.265ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day08/index.js
@@ -0,0 +1,112 @@
+import run from 'aocrunner'
+
+const parseInput = (rawInput, part2 = false) => {
+    rawInput = rawInput.split('\n')
+
+    let instructions = rawInput.shift()
+    let currentPositions = []
+    let destinations = {}
+
+    for (let line of rawInput) {
+        if (!line) continue
+
+        line = line
+            .replace('(', '')
+            .replace(')', '')
+            .replaceAll(' ', '')
+            .split('=')
+
+        let pos = line[0]
+        destinations[pos] = line[1].split(',')
+
+        if (pos === 'AAA' || (part2 && pos.endsWith('A')))
+            currentPositions.push(pos)
+    }
+
+    return { instructions, destinations, currentPositions }
+}
+
+const part1 = (rawInput) => {
+    return getSteps(...Object.values(parseInput(rawInput)))[0]
+}
+
+const part2 = (rawInput) => {
+    return getSteps(...Object.values(parseInput(rawInput, true)), true).reduce(
+        lcm
+    )
+}
+
+const getSteps = (instructions, destinations, currentPositions, part2) => {
+    let steps = []
+
+    for (let i = 0; i < currentPositions.length; i++) {
+        let currPos = currentPositions[i],
+            numOfSteps = 0
+
+        while (true) {
+            let intermediateStep =
+                destinations[currPos][
+                    instructions[numOfSteps % instructions.length] === 'L'
+                        ? 0
+                        : 1
+                ]
+
+            numOfSteps++
+
+            if (
+                intermediateStep === 'ZZZ' ||
+                (part2 && intermediateStep.endsWith('Z'))
+            ) {
+                break
+            }
+
+            currPos = intermediateStep
+        }
+
+        steps.push(numOfSteps)
+    }
+
+    return steps
+}
+
+const gcd = (a, b) => (a ? gcd(b % a, a) : b)
+
+const lcm = (a, b) => (a * b) / gcd(a, b)
+
+run({
+    part1: {
+        tests: [
+            {
+                input: `
+                LLR
+
+                AAA = (BBB, BBB)
+                BBB = (AAA, ZZZ)
+                ZZZ = (ZZZ, ZZZ)`,
+                expected: 6
+            }
+        ],
+        solution: part1
+    },
+    part2: {
+        tests: [
+            {
+                input: `
+                LR
+
+                11A = (11B, XXX)
+                11B = (XXX, 11Z)
+                11Z = (11B, XXX)
+                22A = (22B, XXX)
+                22B = (22C, 22C)
+                22C = (22Z, 22Z)
+                22Z = (22B, 22B)
+                XXX = (XXX, XXX)`,
+                expected: 6
+            }
+        ],
+        solution: part2
+    },
+    trimTestInputs: true,
+    onlyTests: false
+})
